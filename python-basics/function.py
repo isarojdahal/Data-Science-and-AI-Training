cedaa208711819930c5ff3d095479650fee89e23
Saroj Dahal <isarojdahal@gmail.com>
2026-03-19 14:27:53 +0545

    init

---
+++ python-basics/function.py
@@ -0,0 +1,9 @@
+def adder(nums):
+    sum = 0
+    for i in nums:
+        sum+=i # sum = sum + i
+
+    print("sum is : "+str(sum))
+
+
+adder([1,2,3])
---
+++ python-basics/main.py
@@ -0,0 +1,13 @@
+grade = float(input("Enter Grade : "))
+
+if grade>=3.8 and grade<=4:
+    print("A")
+
+elif grade >=3.6 and grade<3.8:
+    print("A-")
+
+elif grade >=3.4 and grade<3.6:
+    print("B")
+
+else:
+    print("C")
